40fa649e3960557e38c2e14e45f895089e52f9f4
David Tabaka <dabakovich@gmail.com>
2023-03-09 16:39:45 +0200

    chore: add envs for optional webhook bot and mongo db usage

--- bot/bot.py
+++ bot/bot.py
@@ -7,7 +7,7 @@ from handlers.error_handler import error_handler
 from handlers.start_command_handler import start_command_handler
 from handlers.text_message_handler import text_message_handler
 
-from config import TELEGRAM_BOT_TOKEN, PORT
+from config import TELEGRAM_BOT_TOKEN, PORT, IS_USE_WEBHOOK_BOT
 
 # ToDo: add to shared util
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -25,11 +25,14 @@ def main() -> None:
 
     application.add_error_handler(error_handler)
 
-    # Start the bot
-    application.run_webhook(listen="0.0.0.0",
-                            port=PORT,
-                            url_path=TELEGRAM_BOT_TOKEN,
-                            webhook_url='https://python-chatgpt-bot.herokuapp.com/' + TELEGRAM_BOT_TOKEN)
+    if IS_USE_WEBHOOK_BOT:
+        # Start the bot
+        application.run_webhook(listen="0.0.0.0",
+                                port=PORT,
+                                url_path=TELEGRAM_BOT_TOKEN,
+                                webhook_url='https://python-chatgpt-bot.herokuapp.com/' + TELEGRAM_BOT_TOKEN)
+    else:
+        application.run_polling()
 
 
 if __name__ == '__main__':
--- bot/config.py
+++ bot/config.py
@@ -9,4 +9,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 DEVELOPER_CHAT_ID = os.getenv("DEVELOPER_CHAT_ID")
 
+IS_USE_MONGO_DB = bool(os.getenv("IS_USE_MONGO", True))
+IS_USE_WEBHOOK_BOT = bool(os.getenv("IS_USE_WEBHOOK_BOT", True))
+
 PORT = int(os.getenv('PORT', 5000))
